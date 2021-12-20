aef513da86053796f6ab0e6083e8bbbf6fec6304
ShanJiang1911 <johnson33399@gmail.com>
2021-12-19 23:14:28 -0800

    add code-challenge15 tree

--- python/tree/binary_search_tree.py
+++ python/tree/binary_search_tree.py
@@ -1,5 +1,30 @@
 from tree.binary_tree import BinaryTree
+from tree.node import Node
 
 
 class BinarySearchTree(BinaryTree):
-  pass
+  def add(self, value):
+    node = Node(value)
+    if self.root is None:
+      self.root = node
+
+    def step(root, node_to_add):
+      if root is None:
+        return
+
+      if node_to_add.value < root.value:
+        if root.left:
+          step(root.left, node_to_add)
+        else:
+          root.left = node_to_add
+
+      else:
+        if root.right:
+          step(root.right, node_to_add)
+        else:
+          root.right = node_to_add
+
+    step(self.root, node)
+
+  def contains(self, value):
+    pass
