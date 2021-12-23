fff65a7fef64e8427fa15d3c0b81af606c63b513
ShanJiang1911 <johnson33399@gmail.com>
2021-12-22 23:53:04 -0800

    add tree-fizz-buzz

---
+++ python/code_challenges/tree_fizz_buzz/tree_fizz_buzz.py
@@ -0,0 +1,28 @@
+class Node:
+  def __init__(self, value):
+    self.value = value
+
+class KaryTree:
+  def __init__(self, root=None):
+    self.root = root
+
+
+
+def fizz_buzz_tree(tree):
+  KaryTreeList = []
+  newKaryTreeList = []
+
+  for i in KaryTreeList:
+    if i.value%3 == 0:
+      i = "Fizz"
+    elif i.value%5 == 0:
+      i = "Buzz"
+    elif i.value%3 == 0 and i.value%5 == 0:
+      i = "FizzBuzz"
+    else:
+      i = str(i)
+    newKaryTreeList.append(i)
+  return newKaryTreeList
+
+
+
