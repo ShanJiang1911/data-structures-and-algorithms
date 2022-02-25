a0bf7d2a597e4050494931758419ad8ef85b5090
ShanJiang1911 <johnson33399@gmail.com>
2022-02-24 22:03:30 -0800

    revise fizz-buzz-tree

--- python/code_challenges/tree_fizz_buzz/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz/tree_fizz_buzz.py
@@ -1,42 +1,33 @@
 class Node:
-  def __init__(self, value):
+  def __init__(self, value=None):
     self.value = value
+    self.childen = []
 
 class KaryTree:
   def __init__(self, root=None):
     self.root = root
 
-
+  def add(self, value):
+    if self.root is None:
+      self.root = Node(value)
+    else:
+      self.root.childen.append(Node(value))
 
 def fizz_buzz_tree(tree):
   KaryTreeList = []
   newKaryTreeList = []
 
-<<<<<<< HEAD
-  # for i in KaryTreeList:
-  #   if i.value%3 == 0:
-  #     i = "Fizz"
-  #   elif i.value%5 == 0:
-  #     i = "Buzz"
-  #   elif i.value%3 == 0 and i.value%5 == 0:
-  #     i = "FizzBuzz"
-  #   else:
-  #     i = str(i)
-  #   newKaryTreeList.append(i)
-  # return newKaryTreeList
-=======
   for i in KaryTreeList:
-    if i.value%3 == 0:
+    if i.value % 3 == 0:
       i = "Fizz"
-    elif i.value%5 == 0:
+    elif i.value % 5 == 0:
       i = "Buzz"
-    elif i.value%3 == 0 and i.value%5 == 0:
+    elif i.value % 3 == 0 and i.value % 5 == 0:
       i = "FizzBuzz"
     else:
       i = str(i)
     newKaryTreeList.append(i)
   return newKaryTreeList
->>>>>>> b1be8ca99b92aec244412aafe6d34071cb3efad2
 
 
 
